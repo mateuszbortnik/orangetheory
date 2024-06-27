4abbf627c48119679a253da1201d4beab9d65b99
mateuszbortnik <128790421+mateuszbortnik@users.noreply.github.com>
2024-06-27 09:53:24 +0000

    f

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 st.title("📊 Data evaluation app")
 
-csv_file = '/workspaces/orangetheory/bquxjob_1c0d9aa4_19055bab559.csv'
+csv_file = 'bquxjob_1c0d9aa4_19055bab559.csv'
 
 # Read the CSV file into a DataFrame
 df = pd.read_csv(csv_file)
